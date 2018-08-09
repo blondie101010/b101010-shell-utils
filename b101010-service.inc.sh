fe8569752a806d3a2ce00626c01336cdfbf713d0
Julie Pelletier <blondie101010@hotmail.com>
2018-08-09 03:06:26 -0400

    fixed formatting

--- b101010-service.inc.sh
+++ b101010-service.inc.sh
@@ -3,7 +3,7 @@
 # Blondie101010's basic shell script library system service utility module.
 
 if [[ $b101010_service > 0 ]]; then     # script already included
-        return 0
+	return 0
 fi
 
 source /usr/local/lib/b101010.inc.sh
@@ -13,112 +13,112 @@ b101010_service=1
 
 # Run the system's init service controller.
 serviceControl() {      # $1:operation, $2:unit, [$3:source filename for install]
-        if [[ "$INIT_SYSTEM" = "" ]]; then
-                initDetect
-        fi
+	if [[ "$INIT_SYSTEM" = "" ]]; then
+		initDetect
+	fi
 
-        case "$1" in
-                start|stop|restart)
-                        case "$INIT_SYSTEM" in
-                                "openrc")
-                                        rc-service $2 $1
-                                ;;
+	case "$1" in
+		start|stop|restart)
+			case "$INIT_SYSTEM" in
+				"openrc")
+					rc-service $2 $1
+				;;
 
-                                "systemd")
-                                        systemctl $1 $2
-                                ;;
+				"systemd")
+					systemctl $1 $2
+				;;
 
-                                "sysv")
-                                        /etc/init.d/$2 $1
-                                ;;
+				"sysv")
+					/etc/init.d/$2 $1
+				;;
 
-                                "sysv-service")
-                                        service $2 $1
-                                ;;
-                        esac
-                ;;
+				"sysv-service")
+					service $2 $1
+				;;
+			esac
+		;;
 
-                install)
-                        TARGET_NAME=$INIT_DIR/$2
+		install)
+			TARGET_NAME=$INIT_DIR/$2
 
-                        if [[ "$INIT_SYSTEM" = "systemd" ]]; then
-                                TARGET_NAME="$TARGET_NAME.service"
-                        fi
+			if [[ "$INIT_SYSTEM" = "systemd" ]]; then
+				TARGET_NAME="$TARGET_NAME.service"
+			fi
 
-                        cp $3 $TARGET_NAME
-                ;;
+			cp $3 $TARGET_NAME
+		;;
 
-                enable)
-                        case "$INIT_ENABLE" in
-                                rc-update)
-                                        rc-update add $2
-                                ;;
+		enable)
+			case "$INIT_ENABLE" in
+				rc-update)
+					rc-update add $2
+				;;
 
-                                systemd)
-                                        systemctl enable $2
-                                ;;
+				systemd)
+					systemctl enable $2
+				;;
 
-                                chkconfig)
-                                        chkconfig $2 on
-                                ;;
+				chkconfig)
+					chkconfig $2 on
+				;;
 
-                                update-rc.d)
-                                        update-rc.d $2 defaults
-                                ;;
+				update-rc.d)
+					update-rc.d $2 defaults
+				;;
 
-                                ln)
-                                        ln -s /etc/init.d/$2 /etc/defaults/.
-                                ;;
-                        esac
-                ;;
+				ln)
+					ln -s /etc/init.d/$2 /etc/defaults/.
+				;;
+			esac
+		;;
 
-                *) error "Invalid service operation ($1)."
-                ;;
-        esac
+		*) error "Invalid service operation ($1)."
+		;;
+	esac
 }
 
 # Detect what to use (rc-service, service, systemctl, etc) to control this system's services.
 # We set 3 variables: INIT_DIR, INIT_SYSTEM, and INIT_ENABLE.
 initDetect() {
-        INIT_DIR=/etc/init.d
+	INIT_DIR=/etc/init.d
 
-        type rc-service > /dev/null 2>&1
+	type rc-service > /dev/null 2>&1
 
-        if [[ $? == 0 ]]; then
-                INIT_SYSTEM=openrc
-                INIT_ENABLE=rc-update
-                return
-        fi
+	if [[ $? == 0 ]]; then
+		INIT_SYSTEM=openrc
+		INIT_ENABLE=rc-update
+		return
+	fi
 
-        type systemctl > /dev/null 2>&1
+	type systemctl > /dev/null 2>&1
 
-        if [[ $? == 0 ]]; then
-                INIT_DIR=/etc/systemd/system
-                INIT_SYSTEM=systemd
-                INIT_ENABLE=systemd
-                return
-        fi
+	if [[ $? == 0 ]]; then
+		INIT_DIR=/etc/systemd/system
+		INIT_SYSTEM=systemd
+		INIT_ENABLE=systemd
+		return
+	fi
 
-        type service > /dev/null 2>&1
-        if [[ $? == 0 ]]; then
-                INIT_SYSTEM=sysv-service
-        else
-                INIT_SYSTEM=sysv
-        fi
+	type service > /dev/null 2>&1
+	if [[ $? == 0 ]]; then
+		INIT_SYSTEM=sysv-service
+	else
+		INIT_SYSTEM=sysv
+	fi
 
-        type chkconfig > /dev/null 2>&1
+	type chkconfig > /dev/null 2>&1
 
-        if [[ $? == 0 ]]; then
-                INIT_ENABLE=chkconfig
-        else
-                type update-rc.d > /dev/null 2>&1
+	if [[ $? == 0 ]]; then
+		INIT_ENABLE=chkconfig
+	else
+		type update-rc.d > /dev/null 2>&1
 
-                if [[ $? == 0 ]]; then
-                        INIT_ENABLE=update-rc.d
-                else
-                        INIT_ENABLE=ln
-                fi
-        fi
+		if [[ $? == 0 ]]; then
+			INIT_ENABLE=update-rc.d
+		else
+			INIT_ENABLE=ln
+		fi
+	fi
 
-        return
+	return
 }
--- b101010.inc.sh
+++ b101010.inc.sh
@@ -4,7 +4,7 @@
 # It will be done progressively based on other open source project requirements.
 
 if [[ $b101010 > 0 ]]; then     # script already included
-        return 0
+	return 0
 fi
 
 
@@ -12,53 +12,53 @@ fi
 b101010=1
 
 # Show a warning.
-warn() {        # $1:message
-        echo -e "\n\e[1;33m$1\e[0m"
+warn() {	# $1:message
+	echo -e "\n\e[1;33m$1\e[0m"
 }
 
 # Output an error message and exit.
 error() {       # $1:errorMessage
-        echo -e "\n\e[1;31m$1\e[0m"
-        exit 1
+	echo -e "\n\e[1;31m$1\e[0m"
+	exit 1
 }
 
 # Prompt for confirmation and abort if the user enters anything other than 'y'.
 confirm() {
-        echo ""
-        read -e -p "Are you sure? [y/N] " yn
+	echo ""
+	read -e -p "Are you sure? [y/N] " yn
 
-        case "$yn" in
-                [yY])
-                        return
-                ;;
+	case "$yn" in
+		[yY])
+			return
+		;;
 
-                *)
-                        error aborting
-                ;;
-        esac
+		*)
+			error aborting
+		;;
+	esac
 }
 
 # Validate the return value and output a warning or error when appropriate.
 checkRet() {    # $1:$?, $2:operation ('W'arn/'E'rror)
-        message="An error occured.  Its description should appear above."
+	message="An error occured.  Its description should appear above."
 
-        if [[ $1 != 0 ]]; then
-                case "$2" in
-                        [wW]) warn "$message";;
-                        [eE]) error "$message";;
-                        *) error "Invalid operation passed to checkRet(): '$2'."
-                esac
-        fi
+	if [[ $1 != 0 ]]; then
+		case "$2" in
+			[wW]) warn "$message";;
+			[eE]) error "$message";;
+			*) error "Invalid operation passed to checkRet(): '$2'."
+		esac
+	fi
 }
 
 # Use return the environment variable requested if specified, otherwise prompt for it.
 getEnvOrPrompt() {      # $1:varName, $2:prompt, [$3:default]
-        _val=${!1}
-        if [[ "$_val" = "" ]]; then
-                echo ""
-                read -e -p "$2" -i "$3" $1
-        else
-                echo "Using predefined $1=$_val.";
-        fi
+	_val=${!1}
+	if [[ "$_val" = "" ]]; then
+		echo ""
+		read -e -p "$2" -i "$3" $1
+	else
+		echo "Using predefined $1=$_val.";
+	fi
 }
 
