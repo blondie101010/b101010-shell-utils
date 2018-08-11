68a3a37549774277537a11593c0bd7d22fc5087c
Julie Pelletier <blondie101010@hotmail.com>
2018-08-10 22:40:28 -0400

    normalize /usr/local

--- install.sh
+++ install.sh
@@ -8,6 +8,12 @@ cd b101010-shell-utils
 wget https://github.com/blondie101010/b101010-shell-utils/archive/master.tar.gz
 
 tar -xzf master.tar.gz
+
+if [[ ! -d /usr/local ]]; then
+	# normalize
+	ln -s /usr /usr/local
+fi
+
 cp b101010-shell-utils-master/b101010* /usr/local/lib/.
 
 rm -rf b101010-shell-utils
